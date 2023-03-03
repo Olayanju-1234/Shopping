187268d0cc8e719dc1a1dd6b1b46dc9d287f3a4c
Olayanju-1234 <horlarhyanjuhjoseph@gmail.com>
2023-03-03 20:40:27 +0100

    Blog category controller

---
+++ controllers/blogcategoryController.js
@@ -0,0 +1,67 @@
+const Blog = require('../models/BlogCategoryModel')
+const { StatusCodes } = require('http-status-codes')
+require('express-async-errors')
+const AppEror = require('../errors/errors')
+const validateMongoId = require('../utils/validateMongoId')
+
+const createCategory = async (req, res) => {
+    const newCategory = await Blog.create(req.body)
+    res.status(StatusCodes.CREATED).json({
+        success: true,
+        newCategory
+    })
+}
+
+const updateCategory = async (req, res) => {
+    const { id } = req.params
+    validateMongoId(id)
+    const updateCategory = await Blog.findByIdAndUpdate(id, {
+        $set: req.body
+    }, { new: true });
+    res.status(StatusCodes.OK).json({
+        success: true,
+        updateCategory
+    })
+}
+
+const getSingleCategory = async (req, res) => {
+    const { id } = req.params
+    validateMongoId(id)
+    const getCategory = await Blog.findById(id)
+    if (!getCategory) {
+        throw new AppEror.NotFoundError("Category not found")
+    }
+    res.status(StatusCodes.OK).json({
+        success: true,
+        getCategory
+    })
+}
+
+const deleteCategory = async (req, res) => {
+    const { id } = req.params
+    validateMongoId(id)
+    const deleteCategory = await Blog.findByIdAndDelete(id)
+    if (!deleteCategory) {
+        throw new AppEror.NotFoundError("Category not found")
+    }
+    res.status(StatusCodes.OK).json({
+        success: true,
+        message: "Category deleted successfully"
+    })
+}
+
+const getAllCategories = async (req, res) => {
+    const categories = await Blog.find()
+    res.status(StatusCodes.OK).json({
+        success: true,
+        categories
+    })
+}
+
+module.exports = { 
+    createCategory,
+    updateCategory,
+    getSingleCategory,
+    getAllCategories,
+    deleteCategory
+}
