7ff9f6634e83c19c38d5824abd8fcd518d0d2175
Olayanju-1234 <horlarhyanjuhjoseph@gmail.com>
2023-08-02 23:31:14 +0100

    Logging

--- src/api/Utils/logger.js
+++ src/api/Utils/logger.js
@@ -2,7 +2,10 @@ const { createLogger, format, transports } = require('winston');
 
 const logger = createLogger({
     level: 'info',
-    format: format.json(),
+    format: format.combine(
+        format.colorize(),
+        format.json(),
+    ),
 
     transports: [
         new transports.File({ filename: 'logs/error.log', level: 'error' }),
