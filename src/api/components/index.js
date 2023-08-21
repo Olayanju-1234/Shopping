d35499ab750f1349ff61818b186d38d0b5f9bfeb
Olayanju-1234 <horlarhyanjuhjoseph@gmail.com>
2023-08-21 12:29:10 +0100

    Index

--- src/api/components/index.js
+++ src/api/components/index.js
@@ -1,19 +1,21 @@
 const express = require('express')
 const router = express.Router();
 
-const authRoute = require('./Auth/authRoute')
-const blogCategoryRoute = require('./Blog/blogCategoryRoute')
-const blogRoute = require('./Blog/blogRoute')
-const brandRoute = require('./Brand/brandRoute')
-const couponRoute = require('./Coupon/couponRoute')
-const productRoute = require('./Product/productRoute')
-const userRoute = require('./User/userRoute')
+const authRoute = require('./Auth/auth.route')
+const blogCategoryRoute = require('./Blog/blog-category.route')
+const blogRoute = require('./Blog/blog.route')
+const brandRoute = require('./Brand/brand.route')
+const couponRoute = require('./Coupon/coupon.route')
+const productCategoryRoute = require('./Product/product-category.route')
+const productRoute = require('./Product/product.route')
+const userRoute = require('./User/user.route')
 
 router.use('/auth', authRoute)
 router.use('/blog-category', blogCategoryRoute)
 router.use('/blog', blogRoute)
 router.use('/brand', brandRoute)
 router.use('/coupon', couponRoute)
+router.use('/product-category', productCategoryRoute)
 router.use('/product', productRoute)
 router.use('/user', userRoute)
 
