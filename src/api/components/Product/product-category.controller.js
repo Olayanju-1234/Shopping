b542c26a350438104786fb5168cb7fb6414effe7
Olayanju-1234 <horlarhyanjuhjoseph@gmail.com>
2023-08-21 12:28:34 +0100

    Product Component Refactored

---
+++ src/api/components/Product/product-category.controller.js
@@ -0,0 +1,65 @@
+const Category = require('./product-category.model')
+const { StatusCodes } = require('http-status-codes')
+const { NotFoundError } = require('../../errors')
+
+const createCategory = async (req, res) => {
+    const newCategory = await Category.create(req.body)
+    res.status(StatusCodes.CREATED).json({
+        success: true,
+        newCategory
+    })
+}
+
+const updateCategory = async (req, res) => {
+    const { id } = req.params
+    
+    const updateCategory = await Category.findByIdAndUpdate(id, {
+        $set: req.body
+    }, { new: true });
+    res.status(StatusCodes.OK).json({
+        success: true,
+        updateCategory
+    })
+}
+
+const getSingleCategory = async (req, res) => {
+    const { id } = req.params
+    
+    const getCategory = await Category.findById(id)
+    if (!getCategory) {
+        throw new NotFoundError("Category not found")
+    }
+    res.status(StatusCodes.OK).json({
+        success: true,
+        getCategory
+    })
+}
+
+const deleteCategory = async (req, res) => {
+    const { id } = req.params
+    
+    const deleteCategory = await Category.findByIdAndDelete(id)
+    if (!deleteCategory) {
+        throw new NotFoundError("Category not found")
+    }
+    res.status(StatusCodes.OK).json({
+        success: true,
+        message: "Category deleted successfully"
+    })
+}
+
+const getAllCategories = async (req, res) => {
+    const categories = await Category.find()
+    res.status(StatusCodes.OK).json({
+        success: true,
+        categories
+    })
+}
+
+module.exports = { 
+    createCategory,
+    updateCategory,
+    getSingleCategory,
+    getAllCategories,
+    deleteCategory
+}
---
+++ src/api/components/Product/product.route.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const router = express.Router();
+
+const { createProduct,
+        getSingleProduct,
+        getAllProducts,
+        updateProduct,
+        deleteProduct,
+        wishlist,
+        rating,
+} = require('./product.controller');
+
+const uploader = require('../../services/upload/cloudinary');
+
+const { isAdmin, 
+        authenticateUser 
+} = require('../../middlewares/authenticate');
+
+router.route('/').
+    post(authenticateUser, isAdmin, uploader.single('images'), createProduct).
+    get(getAllProducts);
+
+router.route('/wishlist').
+    put(authenticateUser, wishlist); 
+
+router.route('/rating').
+    put(authenticateUser, rating);
+
+router.route('/:id').
+    get(getSingleProduct).
+    put(authenticateUser, isAdmin, updateProduct).
+    delete(authenticateUser, isAdmin,deleteProduct);
+
+module.exports = router
+
